258073eee48f23bdc45a7afd868fa72408812483
hpo1988 <36979508+hpo1988@users.noreply.github.com>
2018-05-13 23:38:47 +0700

    Update hpo1988.py

--- hpo1988.py
+++ hpo1988.py
@@ -225,7 +225,7 @@ def getItems(url_path="0", tq="select A,B,C,D,E"):
 			"context_menu": [
 				ClearPlaylists(""),
 			],
-			"label":"[COLOR yellow]CÁM ƠN CÁC BẠN ĐÃ ỦNG HỘ![/COLOR]",
+			"label":"[COLOR yellow]Cám ơn các bạn đã ũng hộ![/COLOR]",
 			"path": "%s/add-playlist" % (pluginrootpath),
 			"thumbnail": "http://userscontent2.emaze.com/images/5bc10631-6b26-4d3a-a56f-bd68522f965c/Slide21_Pic1_636000467320479770.png"
 		}
@@ -359,33 +359,7 @@ def Section(path = "0", tracking_string = "Home"):
 	items = AddTracking(getItems(path))
 	return plugin.finish(items)
 
-@plugin.route('/add-playlist/<tracking_string>')
-def AddPlaylist(tracking_string = "Add Playlist"):
-	sheet_url = plugin.keyboard(heading='Nhập URL của Google Spreadsheet (có hỗ trợ link rút gọn như bit.ly, goo.gl)')
-	if sheet_url:
-		try:
-			resp, content = http.request(sheet_url,"HEAD")
-			sid, gid = re.compile("/d/(.+?)/.+?gid=(\d+)").findall(resp["content-location"])[0]
-			match_passw = re.search('passw=(.+?)($|&)', resp["content-location"])
-			playlists = plugin.get_storage('playlists')
-			name = plugin.keyboard(heading='Đặt tên cho Playlist')
-
-			item = "[[COLOR yellow]%s[/COLOR]] %s@%s" % (name,gid,sid)
-			if match_passw:
-				item += "@@" + match_passw.group(1)
-			if 'sections' in playlists:
-				playlists["sections"] = [item] + playlists["sections"]
-			else:
-				playlists["sections"] = [item]
-			xbmc.executebuiltin('Container.Refresh')
-		except: 
-			line1 = "Vui lòng nhập URL hợp lệ. Ví dụ dạng đầy đủ:"
-			line2 = "http://docs.google.com/spreadsheets/d/xxx/edit#gid=###"
-			line3 = "Hoặc rút gọn: http://bit.ly/xxxxxx hoặc http://goo.gl/xxxxx"
-			dlg = xbmcgui.Dialog()
-			dlg.ok("URL không hợp lệ!!!", line1, line2, line3)
-
-@plugin.route('/acelist/<path>/<tracking_string>')
+ @plugin.route('/acelist/<path>/<tracking_string>')
 def AceList(path = "0", tracking_string = "AceList"):
 	(resp, content) = http.request(
 		path, "GET",
